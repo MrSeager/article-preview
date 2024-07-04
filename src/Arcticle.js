37ad241b55fd8d0069827e46cc93b9f1a3b855b2
MrSeager <seager1993@gmail.com>
2024-07-04 11:43:57 +0300

    update

--- src/Arcticle.js
+++ src/Arcticle.js
@@ -41,15 +41,15 @@ const Arcticle = () => {
                         it felt slightly bare and uninviting. I’ve got some simple tips 
                         to help you make any room feel complete.</p>
                         <Row className='cs-anchor'>
-                            <Col xs={2} lg={1} className='p-0'>
+                            <Col xs={2} lg={1} className='p-0 d-flex flex-column justify-content-center'>
                                 <Image fluid roundedCircle src={ImgAva} alt='avatar' />
                             </Col>
                             <Col xs={8} lg={10} className='ps-4 d-flex flex-column justify-content-center'>
                                 <h2 className='h6 m-0 cs-c cs-fw-7'>Michelle Appleton</h2>
                                 <p className='cs-c-2 m-0'>28 Jun 2020</p>
                             </Col>
-                            <Col xs={2} lg={1} style={{ zIndex: 2 }} className='p-2 cs-anchor'>
-                                <Button onClick={toggleContainer} className='cs-btn my-auto rounded-circle'><Image fluid src={ImgShare} alt='share' className='cs-img-filter' /></Button>
+                            <Col xs={2} lg={1} style={{ zIndex: 2 }} className='p-0 cs-anchor d-flex flex-column justify-content-center'>
+                                <Button onClick={toggleContainer} className='cs-btn rounded-circle p-1'><Image fluid src={ImgShare} alt='share' className='cs-img-filter w-50 h-50' /></Button>
                             </Col>
                             {showContainer && (
                                 <Container data-aos="fade-up" data-aos-offset="50" style={{ zIndex: 1 }} className='cs-share rounded d-flex flex-row align-items-center justify-content-start justify-content-lg-center px-5 py-2 gap-3 shadow'>
